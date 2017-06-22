042e78c3646b5e3691a0fc136e1c37ca062fb633
Martin Donkersloot <martin.donkersloot@outlook.com>
2017-06-22 21:58:46 +0200

    Updated unit tests.

--- test/test-util.js
+++ test/test-util.js
@@ -1,9 +1,10 @@
-var assert=require('assert');
-var Utility = require('../src/util.js');
+'use strict';
 
+const assert    = require('assert');
+const util      = require('../src/util.js');
 
 describe('padNumber(msg)', function(msg) {
-    var tests = [
+    const tests = [
         { arg:  "0" , expected: "00" } ,
         { arg:  "7" , expected: "07" } ,
         { arg: "13" , expected: "13" } ,
@@ -12,11 +13,11 @@ describe('padNumber(msg)', function(msg) {
 
     tests.forEach(function(test) {
         it('Should create a valid string (' + test.arg + ')', function() {
-            assert.deepEqual(test.expected, Utility.padNumber(test.arg));
+            assert.deepEqual(test.expected, util.padNumber(test.arg));
         });
     });
 
-    var stringTests = [
+    const stringTests = [
         { args: [ "13" , "37" ] , expected: "1337" } ,
         { args: [  "0" , "5"  ] , expected: "0005" } ,
         { args: [ "12" , "3"  ] , expected: "1203" } ,
@@ -25,23 +26,7 @@ describe('padNumber(msg)', function(msg) {
 
     stringTests.forEach(function(test) {
         it('Should create a valid string: (' + test.args[0] + " + " + test.args[1] + ') => ' + test.expected + ' ?', function() {
-            assert.deepEqual(test.expected, Utility.padNumber(test.args[0]) + Utility.padNumber(test.args[1]));
-        });
-    });
-});
-
-describe('compareDankTimes(time1, time2)', function(time1, time2) {
-    var tests = [
-        { args: [ { shoutout: "", hour: 13, minute: 37, points: 0 }, { shoutout: "", hour: 13, minute: 37, points: 0 } ], expected: ("">"") },
-        { args: [ { shoutout: "", hour: 10, minute: 0, points: 0 }, {shoutout: "", hour: 9, minute: 10, points: 0} ], expected: 1 },
-        { args: [ { shoutout: "", hour: 16, minute: 0, points: 0 }, {shoutout: "", hour: 17, minute: 5, points: 0} ], expected: -1 },
-        { args: [ { shoutout: "", hour: 15, minute: 30, points: 0 }, { shoutout: "", hour: 15, minute: 20, points:0 } ], expected: 1 },
-        { args: [ { shoutout: "", hour: 15, minute: 15, points: 0 }, { shoutout: "", hour: 15, minute: 30, points: 0 } ], expected: -1 }
-    ];
-
-    tests.forEach(function(test) {
-        it('Should compare correctly.', function() {
-            assert.deepEqual(test.expected, Utility.compareDankTimes(test.args[0], test.args[1]));
+            assert.deepEqual(test.expected, util.padNumber(test.args[0]) + util.padNumber(test.args[1]));
         });
     });
 });
