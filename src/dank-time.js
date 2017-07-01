62424d0838915f000c14e44b78f1ab814fee1675
Martin Donkersloot <martin.donkersloot@outlook.com>
2017-07-01 23:13:14 +0200

    Refactored command.js and dank-time.js

--- src/dank-time.js
+++ src/dank-time.js
@@ -1,52 +1,62 @@
 'use strict';
 
 /**
- * Creates a new dank time object.
- * @param {number} hour The hour at which points can be scored.
- * @param {number} minute The minute at which points can be scored.
- * @param {string[]} texts The texts to shout at the hour:minute to score points.
- * @param {number} points The amount of points to reward or confiscate.
+ * Represents a dank time.
  */
-function DankTime(hour, minute, texts, points = 1) {
+class DankTime {
+
+    /**
+     * Creates a new dank time object.
+     * @param {number} hour The hour at which points can be scored.
+     * @param {number} minute The minute at which points can be scored.
+     * @param {string[]} texts The texts to shout at the hour:minute to score points.
+     * @param {number} points The amount of points to reward or confiscate.
+     */
+    constructor(hour, minute, texts, points = 1) {
+        this.setHour(hour);
+        this.setMinute(minute);
+        this.setPoints(points);
+        this.setTexts(texts);
+    }
 
     /**
      * Sets the hour.
      * @param {number} newhour
      */
-    this.setHour = function (newhour) {
+    setHour(newhour) {
         if (typeof newhour !== 'number' || newhour < 0 || newhour > 23 || newhour % 1 !== 0) {
             throw TypeError('The hour must be a whole number between 0 and 23!');
         }
-        hour = newhour;
+        this._hour = newhour;
     };
 
     /**
      * Sets the minute.
      * @param {number} newminute
      */
-    this.setMinute = function (newminute) {
+    setMinute(newminute) {
         if (typeof newminute !== 'number' || newminute < 0 || newminute > 59 || newminute % 1 !== 0) {
             throw TypeError('The minute must be a whole number between 0 and 59!');
         }
-        minute = newminute;
+        this._minute = newminute;
     };
 
     /**
      * Sets the points.
      * @param {number} newpoints
      */
-    this.setPoints = function (newpoints) {
+    setPoints(newpoints) {
         if (typeof newpoints !== 'number' || newpoints < 1 || newpoints % 1 !== 0) {
             throw TypeError('The points must be a whole number greater than 0!');
         }
-        points = newpoints;
+        this._points = newpoints;
     };
 
     /**
      * Sets the texts.
      * @param {string[]} newtexts
      */
-    this.setTexts = function (newtexts) {
+    setTexts(newtexts) {
         if (!(newtexts instanceof Array) || newtexts.length < 1) {
             throw TypeError('The texts must be a string array containing at least one item!');
         }
@@ -55,87 +65,80 @@ function DankTime(hour, minute, texts, points = 1) {
                 throw TypeError('The texts must contain only strings!');
             }
         });
-        texts = newtexts;
+        this._texts = newtexts;
     };
-
-    // 'Constructor'
-    this.setHour(hour);
-    this.setMinute(minute);
-    this.setPoints(points);
-    this.setTexts(texts);
 
     /**
      * Gets the texts.
      * @returns {string[]}
      */
-    this.getTexts = function () {
-        return texts;
+    getTexts() {
+        return this._texts;
     };
 
     /**
      * Gets the points.
      * @returns {number}
      */
-    this.getPoints = function () {
-        return points;
+    getPoints() {
+        return this._points;
     };
-
 
     /**
      * Gets the minute.
      * @returns {number}
      */
-    this.getMinute = function () {
-        return minute;
+    getMinute() {
+        return this._minute;
     };
 
     /**
      * Gets the hour.
      * @returns {number}
      */
-    this.getHour = function () {
-        return hour;
+    getHour() {
+        return this._hour;
     };
 
     /**
      * Used by JSON.stringify. Returns a literal representation of this.
      * @return {Object}
      */
-    this.toJSON = function () {
-        return { hour: hour, minute: minute, texts: texts, points: points };
+    toJSON() {
+        return { hour: this._hour, minute: this._minute, texts: this._texts, points: this._points };
+    };
+
+    /**
+     * Returns a new DankTime parsed from a literal.
+     * @param {Object} literal
+     * @returns {DankTime}
+     */
+    static fromJSON(literal) {
+        return new DankTime(literal.hour, literal.minute, literal.texts, literal.points);
+    };
+
+    /**
+     * Compares two dank times using their hour and minute. Used for sorting collections.
+     * @param {DankTime} a
+     * @param {DankTime} b
+     * @returns {number}
+     */
+    static compare(a, b) {
+        if (a.getHour() < b.getHour()) {
+            return -1;
+        }
+        if (a.getHour() > b.getHour()) {
+            return 1;
+        }
+        if (a.getMinute() < b.getMinute()) {
+            return -1;
+        }
+        if (a.getMinute() > b.getMinute()) {
+            return 1;
+        }
+        return 0;
     };
 }
-
-/**
- * Returns a new DankTime parsed from a literal.
- * @param {Object} literal
- * @returns {DankTime}
- */
-DankTime.fromJSON = function (literal) {
-    return new DankTime(literal.hour, literal.minute, literal.texts, literal.points);
-};
-
-/**
- * Compares two dank times using their hour and minute. Used for sorting collections.
- * @param {DankTime} a
- * @param {DankTime} b
- * @returns {number}
- */
-DankTime.compare = function (a, b) {
-    if (a.getHour() < b.getHour()) {
-        return -1;
-    }
-    if (a.getHour() > b.getHour()) {
-        return 1;
-    }
-    if (a.getMinute() < b.getMinute()) {
-        return -1;
-    }
-    if (a.getMinute() > b.getMinute()) {
-        return 1;
-    }
-    return 0;
-};
 
 // Exports.
 module.exports = DankTime;
