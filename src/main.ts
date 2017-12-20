400fd23eb158180310693ed0ee5654c147a2d271
Martin Donkersloot <martin.donkersloot@outlook.com>
2017-12-20 11:54:15 +0100

    Updated dependencies, added warning suppression.

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+// Suppress promise warning from node-telegram-bot-api (https://github.com/yagop/node-telegram-bot-api/issues/319)
+process.env.NTBA_FIX_319 = "1";
+
 import { Chat } from "./chat/chat";
 import * as contextRoot from "./context-root";
 import { TelegramBotCommand } from "./telegram-bot-command/telegram-bot-command";
