b515c241bbd5cbe0e3f52b8a0d21cd73d69adf3d
Lerke <willemsporen@gmail.com>
2018-01-22 23:33:06 +0100

    Deleted unfinished chat-settings code

--- src/chat-setting/validation.ts
+++
@@ -1,7 +0,0 @@
-/** A simple message resulting from a validation check on a value fed to a setting. */
-export interface Validation {
-  /** Whether the value was succesfully validated, and thus the setting value was set. */
-  readonly success: boolean;
-  /** The corresponding message, containing feedback that elaborates on the above value. */
-  readonly message: string;
-}
