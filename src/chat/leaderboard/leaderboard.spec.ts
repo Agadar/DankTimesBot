60f6ceb03ca210d3cd2023de7729b7f669cd4b14
Martin Donkersloot <martin.donkersloot@outlook.com>
2018-08-26 17:30:35 +0200

    Expanded leaderboard unit test to verify issue #93 is already solved.

--- src/chat/leaderboard/leaderboard.spec.ts
+++ src/chat/leaderboard/leaderboard.spec.ts
@@ -5,18 +5,20 @@ import { User } from "../user/user";
 
 describe("Leaderboard.constructor", () => {
 
-  it("should have created an ordered leaderboard", () => {
+  it.only("should have created an ordered leaderboard", () => {
     const users = [
       new User(0, "user0", 15),
       new User(1, "user1", 5),
       new User(2, "user2", 20),
       new User(3, "user3", 10),
+      new User(4, "user4", 20),
     ];
     const leaderboard = new Leaderboard(users);
     assert.equal(leaderboard.entries[0].id, users[2].id);
-    assert.equal(leaderboard.entries[1].id, users[0].id);
-    assert.equal(leaderboard.entries[2].id, users[3].id);
-    assert.equal(leaderboard.entries[3].id, users[1].id);
+    assert.equal(leaderboard.entries[1].id, users[4].id);
+    assert.equal(leaderboard.entries[2].id, users[0].id);
+    assert.equal(leaderboard.entries[3].id, users[3].id);
+    assert.equal(leaderboard.entries[4].id, users[1].id);
   });
 });
 
